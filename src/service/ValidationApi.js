f5aed445ada320acac9b00c6e05f183ead2e32c4
MaJogi <47116426+MaJogi@users.noreply.github.com>
2022-02-23 08:48:17 +0200

    Minor refactoring

--- src/service/ValidationApi.js
+++ src/service/ValidationApi.js
@@ -1,18 +1,13 @@
 import axios from 'axios'
 
-const VALIDATIONS_REST_API_URL_PROCESS_ID =
-  'http://localhost:8080/api/processIdCode'
-const VALIDATIONS_REST_API_URL_VALIDATIONS =
-  'http://localhost:8080/api/validations'
+const apiUrl = 'http://localhost:8080/api'
+
+const VALIDATIONS_REST_API_URL_PROCESS_ID = apiUrl + '/validation/processIdCode'
+const VALIDATIONS_REST_API_URL_VALIDATIONS = apiUrl + '/validation'
 
 class ValidationApi {
   processIdCode(data) {
-    return axios.post(VALIDATIONS_REST_API_URL_PROCESS_ID, data, {
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: '*/*',
-      },
-    })
+    return axios.post(VALIDATIONS_REST_API_URL_PROCESS_ID, data)
   }
   getValidations() {
     return axios.get(VALIDATIONS_REST_API_URL_VALIDATIONS)
